584a82037a951bdaa12d8f0b0622d804853526d2
truongatv <nguyentientruong95@gmail.com>
2019-10-28 23:13:23 +0900

    add server to project

---
+++ server/config/dev.js
@@ -0,0 +1,5 @@
+module.exports = {
+  DB_URI: 'mongodb+srv://truongatv:truongatv@vue-meetup-ow8g1.mongodb.net/test?retryWrites=true&w=majority',
+  SESSION_SECRET: 'sd88d!+asd7jbhxbhj',
+  JWT_SECRET: 'JASD_skdak+12e'
+}
