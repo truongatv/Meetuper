d41117005371f2328e6150d01b3e304ac0b4e447
Truong <truong@c-connect.co.jp>
2019-10-31 14:17:16 +0900

    call api meetup

--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,18 @@
 import Vuex from 'vuex'
 import Vue from 'vue-native-core'
 import axios from 'axios'
+import { Platform } from 'react-native'
 
 Vue.use(Vuex)
+
+const BASE_URL = Platform.OS === 'ios' ? 'http://localhost:3001/api/v1'
+                                       : 'http://10.0.3.2:3001/api/v1'
 
 export default new Vuex.Store({
     //Like data in component. keeping our data in the state
     state: {
-        todos: []
+        todos: [],
+        meetups: []
     },
     //Like computed properties in components.
     //You can use getters to access state in the store
@@ -17,11 +22,19 @@ export default new Vuex.Store({
     //Like methods in components. 
     //To perform actions that usually results in data 
     actions: {
-        async fetchTodo (context) {
+        async fetchTodo ({commit, state}) {
             const res = await axios.get('https://jsonplaceholder.typicode.com/todos');
             const todos = res.data;
-            context.commit('setTodos', todos);
-            return context.state.todos;
+            commit('setTodos', todos);
+            return state.todos;
+        },
+        fetchMeetups ({commit, state}) {
+            return axios.get(`${BASE_URL}/meetups`)
+                .then(res => {
+                    const meetups = res.data
+                    commit('setMeetups', meetups)
+                    return state.meetups
+                })
         }
     }, 
     //Like methods in components.
@@ -29,6 +42,9 @@ export default new Vuex.Store({
     mutations: {
         setTodos (state, todos) {
             Vue.set(state, 'todos', todos)
+        },
+        setMeetups (state, meetups) {
+            Vue.set(state, 'meetups', meetups)
         }
     }
 })
